aee4fd13be1faa38b39721d354f70efcd5e3309a
KarenHovnikyan <hovnikyank@gmail.com>
2020-03-03 13:37:03 +0400

    delete constructor Produserconsumer

--- src/com/company/ProduserConsumer.java
+++ src/com/company/ProduserConsumer.java
@@ -5,27 +5,21 @@ class ProduserConsumer {
     private static int balance = 0;
     private static boolean isvalid;
 
-    ProduserConsumer() {
-    }
-
     void producer() throws InterruptedException {
         while(true) {
             if (!isvalid && balance == 0) {
                 balance += 10;
                 System.out.println(balance);
             }
-
             isvalid = true;
         }
     }
-
     void consumer() throws InterruptedException {
         while(true) {
             if (isvalid && balance == 10) {
                 balance -= 10;
                 System.out.println(balance);
             }
-
             isvalid = false;
         }
     }
