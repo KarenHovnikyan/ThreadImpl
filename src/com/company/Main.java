fd294da3601c89d3146c83e1b0e90be7bcec3a1e
KarenHovnikyan <hovnikyank@gmail.com>
2020-03-03 13:37:46 +0400

    delete probels

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 
         ProduserConsumer pc = new ProduserConsumer();
-
         Thread t1 = new Thread(new Runnable() {
             @Override
             public void run() {
